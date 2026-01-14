2bc24adead46371f0389145170fdbdc1d5a6f901
Roshani5476 <roshdahire1@gmail.com>
2026-01-14 14:57:12 +0530

    WHERE filtering practice
    Added SQL examples demonstrating the use of WHERE clause for filtering data.

---
+++ day01_where.sql
@@ -0,0 +1,23 @@
+--Why: I learned WHERE so that I can filter rows and see only required data
+
+--WHERE filters rows, not columns
+
+--Example: filter by number
+SELECT *FROM demo WHERE ID = 3;
+
+--Example: filter by exact text
+-- text always written in quotes
+SELECT * FROM demo WHERE Name = 'Import';
+
+--LIKE is used for partial text matching 
+--% means anything before or after
+SELECT * FROM demo WHERE Name LIKE '%sql%';
+
+--AND: all conditions must be true
+SELECT * FROM demo WHERE ID > 2 AND Name LIKE '%sql%';
+
+--OR: any one condition can be true
+SELECT * FROM demo WHERE ID = 1 OR ID = 2;
+
+--Memory: WHERE = rows chhanto
+--NEXT: Learn ORDER BY to sort data
