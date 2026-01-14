d02ae0e5a71e71df45c20e3f61420fdfb3508284
Roshani5476 <roshdahire1@gmail.com>
2026-01-14 17:11:44 +0530

    Count = how many rows?
    Added SQL queries demonstrating the use of COUNT function with various conditions and renaming result columns.

---
+++ day01.6_count.sql
@@ -0,0 +1,16 @@
+--WHY: I learned COUNT so that I can answer "how many?" questions
+
+--COUNT(*) counts total rows 
+SELECT COUNT(*) FROM demo;
+
+--COUNT with condition
+SELECT COUNT(*) FROM demO WHERE name LIKE '%sql%'
+
+--COUNT(column) ignores NULL values 
+SELECT COUNT(name) FROM demo
+
+--Use AS to rename result column 
+SELECT COUNT(*) AS total_sql_rows from demo WHERE name LIKE '%sql%'
+
+--MEMORY: COUNT = kitne hai
+--NEXT: Combine SELECT + WHERE + ORDER BY + LIMIT + COUNT
