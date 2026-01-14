d7455a872f47a30ffce962e6e054d69ee4042ceb
Roshani5476 <roshdahire1@gmail.com>
2026-01-14 23:09:18 +0530

    Refine comments in day01.1_select_and_pragma.sql
    Updated comments to clarify SQL commands and their purposes.

--- day01.1_ select_and_pragma.sql
+++ day01.1_ select_and_pragma.sql
@@ -1,16 +1,17 @@
----Why: View data from tables
+---Why: I use _ to see all the data from the table
 SELECT * FROM demo;
 
----Why: I learned selecting specific columns to avoid unnecessary data
+---Why: I learned that instead of selecting all data, we should select only required columns. So, I use _ to get only the Name column.
 SELECT Name FROM demo;
 
----Why: Schema / Structure of table (not data)
+---Why: I use _ to see the structure of the table, not the data
+-- This command shows column names, data types, and primary key information
 PRAGMA table_info(demo);
 
----Structure of the table:
----cid: Column index (internal, starts from 0)
----name: Column name
----type: data type of the column
----notnull:  0 = NULL allowed, 1 = NULL not allowed
----dflt_value: default value (NULL = no default set)
----pk: 1= Primary key, 0 = not primary key
+---In the table structure
+---cid: means column number (internal, starts from 0)
+---name: means column name
+---type: means data type
+---notnull: shows whether null values are allowed ( 0 = NULL allowed, 1 = NULL not allowed)
+---dflt_value: shows the default value (NULL = no default set)
+---pk: shows whether the column is primary key. (1= Primary key, 0 = not primary key)
